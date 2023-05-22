1a16df76b5bceb77867547c863b73b52ee28fdbc
flowflame <3492380787@qq.com>
2023-05-23 07:41:43 +0800

    Third Commit 变动： 1.更新了代码生成器的vm并进行了测试 2.单表和树表模板均可正常运行，操作等无报错。树表基类已进行适配。 注: 主子表可自行适配,有空博主也会更新的 建立新模块是记得在ruoyi-framework模块和ruoyi主maven配置里写入。新模块的pom可复制ruoyi-system模块。

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/TreeEntity.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/TreeEntity.java
@@ -1,5 +1,9 @@
 package com.ruoyi.common.core.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +12,9 @@ import java.util.List;
  * 
  * @author ruoyi
  */
+
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class TreeEntity extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
@@ -25,55 +32,7 @@ public class TreeEntity extends BaseEntity
     private String ancestors;
 
     /** 子部门 */
+    @TableField(exist = false)
     private List<?> children = new ArrayList<>();
 
-    public String getParentName()
-    {
-        return parentName;
-    }
-
-    public void setParentName(String parentName)
-    {
-        this.parentName = parentName;
-    }
-
-    public Long getParentId()
-    {
-        return parentId;
-    }
-
-    public void setParentId(Long parentId)
-    {
-        this.parentId = parentId;
-    }
-
-    public Integer getOrderNum()
-    {
-        return orderNum;
-    }
-
-    public void setOrderNum(Integer orderNum)
-    {
-        this.orderNum = orderNum;
-    }
-
-    public String getAncestors()
-    {
-        return ancestors;
-    }
-
-    public void setAncestors(String ancestors)
-    {
-        this.ancestors = ancestors;
-    }
-
-    public List<?> getChildren()
-    {
-        return children;
-    }
-
-    public void setChildren(List<?> children)
-    {
-        this.children = children;
-    }
 }
